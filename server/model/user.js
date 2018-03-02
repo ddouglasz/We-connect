a6348b4c6e1e13ea4a6b42a97296ab4fa0e783f4
ddouglasz <stevenzenne@gmail.com>
2018-03-02 17:15:46 +0100

    create dummy-data in model folder for businesses

---
+++ server/model/user.js
@@ -0,0 +1,31 @@
+const business = [{
+  id: 1,
+  name: 'andela',
+  description: 'a software development company changing the face of africa',
+  category: 'ICT',
+  quarters: 'lagos',
+  email: 'andela@andela.com',
+  reviews: [],
+},
+{
+  id: 2,
+  name: 'irokotv',
+  description: 'online and offline entertainment business',
+  category: 'entertainment',
+  quarters: 'portharcourt',
+  email: 'iroko@iroko.com',
+  reviews: [],
+},
+{
+  id: 3,
+  name: 'farm crowdy',
+  description: 'connects farm owners with investors on farm produce to share profits',
+  category: 'farming',
+  quarters: 'kaduna',
+  email: 'farmcrowdy@farmcrowdy.com',
+  reviews: [],
+},
+];
+
+export default business;
+
