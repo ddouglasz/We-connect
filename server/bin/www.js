667376fdd1b35e5dcd9e0cf39cfe86342f63c200
ddouglasz <stevenzenne@gmail.com>
2018-03-03 15:18:21 +0100

    finalize setups

---
+++ server/bin/www.js
@@ -0,0 +1,15 @@
+// This will be our application entry. We'll setup our server here.
+// const http = require('http');
+// const app = require('./server/app'); // The express app we just created
+
+import * as http from 'http';
+import app from '../../app';
+
+const port = parseInt(process.env.PORT, 10) || 8000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port);
+
+console.log('🤡 let the awesomeness begin ♣️ ♥️');
+
