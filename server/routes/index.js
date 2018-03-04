50f4a9b4da15f497fb6bb1c5c5ccc2e71ca04e5a
ddouglasz <stevenzenne@gmail.com>
2018-03-04 10:54:13 +0100

    delete unwanted routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,13 +10,5 @@ import businesses from '../model/business'; // eslint-disable-line no-unused-var
 
 export default (app) => {
   app.post('/api/v1/businesses', Businesses.createBusinesses);
-  app.put('/api/v1/businesses/:businessId', Businesses.updateBusinesses);
-  app.get('/api/v1/businesses', Businesses.getBusinesses);
-  app.delete('/api/v1/businesses/:businessId', Businesses.removeBusinesses);
-  app.get('/api/v1/businesses/:businessId', Businesses.retrieveBusiness);
-  app.post('api/v1/businesses/:businessId/reviews', Businesses.postReview);
-  app.get('businesses/v1/:businessId/reviews', Businesses.getReviews);
-  app.post('/api/v1/auth/users/signup', Users.signUp);
-  app.post('/api/v1/auth/users/signin', Users.signIn);
-};
+ };
 
