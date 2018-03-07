5dece4a86f177e9173d7466430158a44839f6f2b
ddouglasz <stevenzenne@gmail.com>
2018-03-07 13:20:57 +0100

    chore:write middleware methods to handle errors cases in post business routes

--- server/middlewares/createBusinessValidator.js
+++ server/middlewares/createBusinessValidator.js
@@ -1,50 +1,65 @@
+import validator from 'validator';
+
 /**
- * @returns {Object} validate Input
- * @param {*} req
- * @param {*} res
- * @param {*} next
+ *
+ *
+ * @class Validator
  */
-const validateInput = (req, res, next) => {
-  if (!req.body.name) {
-    return res.status(400).send({
-      message: 'business name cannot be empty',
-    });
+class Validator {
+  /**
+     *
+     *
+     * @param {any} req
+     * @param {any} res
+     * @param {any} next
+     * @returns {json} validate business registeration
+     */
+  static addBusinessValidator(req, res, next) {
+    const {
+      name, description, category, location
+    } = req.body;
+    const errors = {};
+    if (name === undefined || description === undefined || category === undefined
+            || location === undefined) {
+      res.status(400)
+        .json({
+          message: 'All or some of the field is/are undefined',
+        });
+    } else {
+      // check for name
+      if (!validator.isEmpty(name)) {
+        if (!validator.isLength(name, { min: 3, max: 50 })) {
+          errors.name = 'Name of business must between 3 to 50 characters';
+        }
+      } else {
+        errors.name = 'Name of business is required';
+      }
+
+      // check for description
+      if (!validator.isEmpty(description)) {
+        if (!validator.isLength(description, { min: 10, max: 300 })) {
+          errors.description = 'description of business must between 20 to 500 characters';
+        }
+      } else {
+        errors.description = 'description of business is required';
+      }
+
+      // check for category
+      if (validator.isEmpty(category)) {
+        errors.category = 'category can not be empty';
+      }
+
+      // check for location
+      if (validator.isEmpty(location)) {
+        errors.location = 'location can not be empty';
+      }
+
+      if (Object.keys(errors).length !== 0) {
+        return res.status(400)
+          .json(errors);
+      } next();
+    }
   }
-  if (!req.body.id) {
-    return res.status(400).send({
-      message: 'id cannot be empty',
-    });
-  }
-  if (!req.body.description) {
-    return res.status(400).send({
-      message: 'description cannot be empty',
-    });
-  }
-  if (!req.body.category) {
-    return res.status(400).send({
-      message: 'category cannot be empty',
-    });
-  }
-  if (!req.body.location) {
-    return res.status(400).send({
-      message: 'location cannot be empty',
-    });
-  }
-  if (!req.body.email) {
-    return res.status(400).send({
-      message: 'email cannot be empty',
-    });
-  }
-  if (!req.body.image) {
-    return res.status(400).send({
-      message: 'image cannot be empty',
-    });
-  }
-  if (!(req.body.description.join('').match('/^.{8,}$/'))) {
-    return res.status(400).send({
-      message: 'description should be greater than 8 characters Request',
-    });
-  }
-  next();
-};
-export default validateInput;
+}
+
+export default Validator;
