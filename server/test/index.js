3739d4bd0ff38aa545487bb2a1d0f1bccea6475e
ddouglasz <stevenzenne@gmail.com>
2018-03-08 13:39:50 +0100

    chore: write test for posting a business

--- server/test/index.js
+++ server/test/index.js
@@ -16,7 +16,7 @@ describe('Test API', () => {
         done();
       });
   });
-  // Test for getting undefind routes
+  // Test for getting undefind rouotes
   it('Should return 404 for routes not specified', (done) => {
     chai.request(app)
       .get('/blah/blah/businesses')
@@ -25,7 +25,7 @@ describe('Test API', () => {
         done();
       });
   });
-  // Test for posting to undefind routes
+  // Test for posting to undefind rouotes
   it('Undefined Routes Should Return 404', (done) => {
     chai.request(app)
       .post('/another/blahblah/business')
@@ -36,5 +36,28 @@ describe('Test API', () => {
       });
   });
 });
+
+// Test for posting a business
+describe('POST business', () => {
+  it('Should return 200 for a sucessful post', (done) => {
+    chai.request(app)
+      .post('/api/v1/businesses')
+      .send({
+        id: 5,
+        name: 'Andela Nigeria',
+        image: 'steve.jpg',
+        description: 'great company with acute vision and a focus for distributed yada yada yada blah blah blah🤡',
+        category: 'ICT',
+        location: 'Lagos',
+        email: 'steve@steve.com'
+      })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+
+ 
 
  
