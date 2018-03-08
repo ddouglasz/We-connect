4ab9486daca69a3e2ffa380473c478cf917d2b1b
ddouglasz <stevenzenne@gmail.com>
2018-03-08 18:59:43 +0100

    chore: create route for signup controller

--- server/model/user.js
+++ server/model/user.js
@@ -1,24 +1,28 @@
 
 const users = [{
   userId: 1,
+  fullname: 'clinton clinton',
   userName: 'clinton',
   password: 'clint222',
   email: 'clint@fidel.com'
 },
 {
   userId: 1,
+  fullname: 'seun seun',
   userName: 'seun',
   password: 'seunzone',
   email: 'seun@zone.com'
 },
 {
   userId: 1,
+  fullname: 'pascal pascal',
   userName: 'pascal11',
   password: 'passpass',
   email: 'pas@cal.com'
 },
 {
   userId: 1,
+  fullname: 'longe longe',
   userName: 'longe',
   password: 'longman',
   email: 'long@man.com'
