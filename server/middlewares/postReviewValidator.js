a05ab0519a1f477aef16c17af46feaee2cbc5906
ddouglasz <stevenzenne@gmail.com>
2018-03-08 13:27:17 +0100

    chore:modify post business middleware to pass written test

--- server/middlewares/postReviewValidator.js
+++ server/middlewares/postReviewValidator.js
@@ -15,39 +15,35 @@ class ReviewsValidator {
      * @returns {json} validate business registeration
      */
   static postReviewValidator(req, res, next) {
-    const {
-      id, reviewedBy, reviews
-    } = req.body;
+    const { id, reviewedBy, review } = req.body;
     const errors = {};
-    if (id === undefined || reviewedBy === undefined || reviews === undefined
+    if (id === undefined || reviewedBy === undefined || review === undefined
     ) {
-      res.status(400)
-        .json({
-          message: 'All or some of the field is/are undefined',
-        });
-    } else {
-      // check for id
-      if (!validator.isEmpty(reviewedBy)) {
-        if (!validator.isLength(reviewedBy, { min: 3, max: 30 })) {
-          errors.name = 'reviewers name should be between 3 to 50 characters';
-        }
-      } else {
-        errors.name = 'Name of of reviewer is required';
-      }
-
-      // check for reviews
-      if (!validator.isEmpty(reviews)) {
-        if (!validator.isLength(reviews, { min: 20, max: 300 })) {
-          errors.description = 'reviews of a business must be between 20 to 300 characters';
-        }
-      } else {
-        errors.description = 'reviews can not be empty';
-      }
-      if (Object.keys(errors).length !== 0) {
-        return res.status(400)
-          .json(errors);
-      } next();
+      return res.status(400).json({
+        message: 'All or some of the field is/are undefined',
+      });
     }
+    // check for reviwedBy
+    if (validator.isEmpty(reviewedBy)) {
+      errors.reviewedBy = 'Name of of reviewer is required';
+    } else if (!validator.isLength(reviewedBy, { min: 3, max: 30 })) {
+      errors.reviewedBy = 'reviewers name should be between 3 to 50 characters';
+    }
+
+    // check for reviews
+    if (!validator.isEmpty(review)) {
+      errors.description = 'reviews can not be empty';
+    } else if (!validator.isLength(review, { min: 20, max: 300 })) {
+      errors.description = 'reviews of a business must be between 20 to 300 characters';
+    }
+    // check for id
+    if (validator.isEmpty(id)) {
+      errors.id = 'id can not be empty';
+    }
+    if (Object.keys(errors).length !== 0) {
+      return res.status(400)
+        .json(errors);
+    } next();
   }
 }
 
