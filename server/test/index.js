6bb121e3692383210e438d0677056f17d1a7f79f
ddouglasz <stevenzenne@gmail.com>
2018-03-08 13:37:56 +0100

    chore: write test for a default route

--- server/test/index.js
+++ server/test/index.js
@@ -6,4 +6,14 @@ const { expect } = chai;
 
 chai.use(chaiHttp);
 
- 
+describe('Test API', () => {
+  // Test for default route
+  it('Should return 200 for the default route', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        done();
+      });
+  });
+});
