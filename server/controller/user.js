9ab6510d82fa1d5a8c434962f4562f00fd6535b8
ddouglasz <stevenzenne@gmail.com>
2018-03-08 19:01:57 +0100

    chore:write controller method for sign up user

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,6 +1,27 @@
-// /**
-//  * class Users
-//  */
-// static signUp(req,res){
-    
-// }
+import users from '../model/user';
+
+import bcrypt from 'bcrypt';
+/**
+ * class Users
+ */
+class Users {
+  /**
+   * @returns {Object} signUp
+   * @param {*} req
+   * @param {*} res
+   */
+  static signUp(req, res) {
+    users.push({
+      userId: users.length + 1,
+      fullname: req.body.fullname,
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    return res.status(200).json({
+      message: 'user succesfully created'
+    });
+  }
+}
+
+export default Users;
