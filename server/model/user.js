f13740f03d993bf2355c4e81c12c5dc6781901d0
ddouglasz <stevenzenne@gmail.com>
2018-03-09 15:11:20 +0100

    chore:rename users params in model

--- server/model/user.js
+++ server/model/user.js
@@ -2,28 +2,28 @@
 const users = [{
   userId: 1,
   fullname: 'clinton clinton',
-  userName: 'clinton',
+  username: 'clinton',
   password: 'clint222',
   email: 'clint@fidel.com'
 },
 {
   userId: 1,
   fullname: 'seun seun',
-  userName: 'seun',
+  username: 'seun',
   password: 'seunzone',
   email: 'seun@zone.com'
 },
 {
   userId: 1,
   fullname: 'pascal pascal',
-  userName: 'pascal11',
+  username: 'pascal11',
   password: 'passpass',
   email: 'pas@cal.com'
 },
 {
   userId: 1,
   fullname: 'longe longe',
-  userName: 'longe',
+  username: 'longe',
   password: 'longman',
   email: 'long@man.com'
 },
