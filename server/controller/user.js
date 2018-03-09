5eba4769fc709dab2b5c1990ddd0ac2e5f919933
ddouglasz <stevenzenne@gmail.com>
2018-03-09 14:23:30 +0100

    chore:write controlleer for signing in users

--- server/controller/user.js
+++ server/controller/user.js
@@ -24,6 +24,23 @@ class Users {
       message: 'user succesfully created'
     });
   }
+  /**
+   * @returns {Object} signUp
+   * @param {*} req
+   * @param {*} res
+   */
+  static signIn(req, res) {
+    for (let i = 0; i < users.length; i += 1) {
+      if (users[i].username === req.body.username && users[i].password === req.body.password) {
+        return res.status(200).json({
+          message: 'user succesfully signed in'
+        });
+      }
+    }
+    return res.status(401).send({
+      message: 'username and/password is not correct'
+    });
+  }
 }
 
 export default Users;
