d84e9910cc4657bf628b80cf98a5ac28d79b696e
ddouglasz <stevenzenne@gmail.com>
2018-03-10 03:08:37 +0100

    chore(bugfix):remove commented out codes

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,7 +1,3 @@
-// This will be our application entry. We'll setup our server here.
-// const http = require('http');
-// const app = require('./server/app'); // The express app we just created
-
 import * as http from 'http';
 import app from '../../app';
 
