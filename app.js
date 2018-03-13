3d8690aeaeb5bca80464dbab8412f920e53a158e
ddouglasz <stevenzenne@gmail.com>
2018-03-13 15:01:37 +0100

    bugfix:fix modify app.js issues

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ app.get('/', (req, res) => res.status(200).send({
 }));
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log(`connected on ${port}`);
-})
+app.listen(port, () => console.log(`connected on ${port}`));
 
 export default app;
 
