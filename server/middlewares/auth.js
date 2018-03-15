b08ad628a295934b0a8ec28bb9f020c9c95b5203
ddouglasz <stevenzenne@gmail.com>
2018-03-15 15:39:29 +0100

    feature: write authentication middleware to allow user to edit businesses

---
+++ server/middlewares/auth.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+import jwtSecret from '../../config';
+
+const secret = jwtSecret.JWT_SECRET;
+/**
+   * @returns {Object} verifies token
+   * @param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
+const verifyToken = (req, res, next) => {
+  const token = req.headers.auth || req.headers['x-access-token'] || req.body.token;
+  if (token) {
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ error: 'Token expired. kindly start a new session' });
+      }
+      req.decoded = decoded;
+      next();
+    });
+  } else {
+    return res.status(401).send({ error: 'Access Denied! kindly login' });
+  }
+};
+
+export default verifyToken;
