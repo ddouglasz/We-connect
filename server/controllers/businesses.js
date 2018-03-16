45e54b4e12a9514ccff8fd94304ef3b7aa39cd22
ddouglasz <stevenzenne@gmail.com>
2018-03-16 02:33:52 +0100

    feature: write cotroller method to create business

---
+++ server/controllers/businesses.js
@@ -0,0 +1,31 @@
+import models from '../models/index';
+
+const BusinessModel = models.businesses;
+// const ReviewsModel = models.reviews;
+
+
+/**
+ * @class Recipe
+ */
+class Businesses {
+/**
+   * @returns {Object} createBusiness
+   * @param {req} req
+   * @param {res} res
+   */
+  static createBusinesses(req, res) {
+    BusinessModel.create({
+      title: req.body.title,
+      description: req.body.description,
+      category: req.body.category,
+      location: req.body.location,
+      email: req.body.email,
+      image: req.body.image,
+      userId: req.decoded.id,
+    })
+      .then(recipe => res.status(201).send(recipe))
+      .catch(error => res.status(400).send(error));
+  }
+}
+
+export default Businesses;
