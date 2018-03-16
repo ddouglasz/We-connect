2c5e48bdfbaaa92eeaf7c97f0b5263a798d26ede
ddouglasz <stevenzenne@gmail.com>
2018-03-16 04:00:29 +0100

    feature: write controller method to get all businesses

--- server/controllers/businesses.js
+++ server/controllers/businesses.js
@@ -9,7 +9,7 @@ const BusinessModel = models.businesses;
  */
 class Businesses {
 /**
-   * @returns {Object} recipes
+   * @returns {Object} business
    * @param {req} req
    * @param {res} res
    */
@@ -18,23 +18,54 @@ class Businesses {
       .then(business => res.status(200).send(business))
       .catch(error => res.status(400).send(error));
   }
-/**
+  /**
    * @returns {Object} createBusiness
    * @param {req} req
    * @param {res} res
    */
   static createBusinesses(req, res) {
-    BusinessModel.create({
-      title: req.body.title,
-      description: req.body.description,
-      category: req.body.category,
-      location: req.body.location,
-      email: req.body.email,
-      image: req.body.image,
-      userId: req.decoded.id,
+    BusinessModel.findOne({
+      where: {
+        title: req.body.title
+      }
+    }).then((business) => {
+      if (business) {
+        return res.status(404).send({
+          message: 'business Already Exists',
+        });
+      }
+      BusinessModel.create({
+        title: req.body.title,
+        description: req.body.description,
+        category: req.body.category,
+        location: req.body.location,
+        email: req.body.email,
+        image: req.body.image,
+        userId: req.decoded.id,
+      })
+        .then(recipe => res.status(201).send(recipe))
+        .catch(error => res.status(400).send(error));
+    });
+  }
+  /**
+   * @returns {Object} retrieveBusiness
+   * @param {req} req
+   * @param {res} res
+   */
+  static retrieveBusiness(req, res) {
+    BusinessModel.findOne({
+      where: {
+        id: req.params.businessId
+      }
+    }).then((business) => {
+      if (!business) {
+        return res.status(404).send({
+          message: 'Business Not Found',
+        });
+      }
+      return res.status(200).send(business);
     })
-      .then(recipe => res.status(201).send(recipe))
-      .catch(error => res.status(400).send(error));
+      .catch(error => res.status(404).send(error));
   }
 }
 
