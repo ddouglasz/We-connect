082e741c61d5ae73fa3ee9495eae08b1b0408746
ddouglasz <stevenzenne@gmail.com>
2018-03-17 11:56:39 +0100

    feature:write controller method to get all reviews for a business

--- server/controllers/businesses.js
+++ server/controllers/businesses.js
@@ -43,7 +43,10 @@ class Businesses {
         image: req.body.image,
         userId: req.decoded.id,
       })
-        .then(busines => res.status(201).send(busines))
+        .then(() => res.status(201).send({
+          message: 'business added successfully',
+          Business: business
+        }))
         .catch(error => res.status(400).send(error));
     });
   }
@@ -76,6 +79,7 @@ class Businesses {
     ReviewsModel.create({
       review: req.body.review,
       businessId: req.params.businessId,
+      userId: req.decoded.id
     })
       .then(() => {
         res.status(201).send({
@@ -85,6 +89,47 @@ class Businesses {
       .catch(() => res.status(400).send({
         message: 'sorry, the business you are trying to review does not exist'
       }));
+  }
+  /**
+   * @returns {Object} getReviews
+   * @param {req} req
+   * @param {res} res
+   */
+  static getReviews(req, res) {
+    BusinessModel
+      .findOne({
+        where: {
+          id: req.params.businessId
+        }
+      })
+      .then((business) => {
+        if (business) {
+          return ReviewsModel
+            .findAll({
+              where: {
+                businessId: req.params.businessId
+              }
+            })
+            .then((reviews) => {
+              if (!reviews) {
+                return res.status(404).json({
+                  message: 'this business does not have any reviews yet,'
+                });
+              }
+              return res.status(200).send({
+                status: 'success',
+                businessdata: {
+                  id: business.id,
+                  title: business.title,
+                  Reviews: reviews
+                }
+              });
+            });
+        }
+        return res.status(404).json({
+          message: 'business not found'
+        });
+      });
   }
 }
 
