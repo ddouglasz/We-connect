befea931b1ae4e9328c48b1292305f6594c5b7f6
ddouglasz <stevenzenne@gmail.com>
2018-03-18 02:17:15 +0100

    feature: write vslidator to check errors for review inputs

--- server/middlewares/postReviewValidator.js
+++ server/middlewares/postReviewValidator.js
@@ -1,10 +1,10 @@
 import validator from 'validator';
 
-import models from '../models/index';
+// import models from '../models/index';
 
-const BusinessModel = models.businesses;
+// const BusinessModel = models.businesses;
 
- /**
+/**
  *
  *
  * @class Validator
@@ -25,25 +25,25 @@ class ReviewsValidator {
           message: 'All or some of the field is/are undefined',
         });
     }
-    // check for a business that does not exist
-    for (let i = 0; i < BusinessModel.length; i += 1) {
-      if (req.param.businessId === BusinessModel[i].businessId) {
-        return res.status(400).json({
-          message: 'business not found',
-        });
-      }
-    }
-    // check for reviwedBy
+    // check for contibutor
     if (validator.isEmpty(req.body.review)) {
       return res.status(400).json({
-        message: 'Name of of reviewer is required',
+        message: 'Name of of contributor is required',
       });
+    }
+    if (!validator.isLength(req.body.review, { min: 10, max: 100 })) {
+      return res.status(406)
+        .send({
+          status: 'Fail',
+          message: 'review content should have between 2 and 100 characters',
+        });
     }
     // check for reviews
     if (validator.isEmpty(req.body.title)) {
-      return res.status(400).json({
-        message: 'review can not be empty',
-      });
+      return res.status(400)
+        .json({
+          message: 'review can not be empty',
+        });
     }
     next();
   }
