889b91d2b18ac7cb4248d61de72a8e4aa9deb694
ddouglasz <stevenzenne@gmail.com>
2018-03-18 02:17:59 +0100

    feature: write vslidator to  check input fields for sign in validator

--- server/middlewares/signUpValidator.js
+++ server/middlewares/signUpValidator.js
@@ -1,4 +1,5 @@
-import isEmail from 'validator/lib/isEmail';
+// import isEmail from 'validator/lib/isEmail';
+// import validator from 'validator';
 /**
    * @returns {Object} validate Input
    * @param {*} req
@@ -21,11 +22,11 @@ const validateUserSignUp = (req, res, next) => {
       message: 'Email is required',
     });
   }
-  if (!isEmail(req.body.email)) {
-    return res.status(400).send({
-      message: 'Email Invalid',
-    });
-  }
+  // if (!isEmail(req.body.email)) {
+  //   return res.status(400).send({
+  //     message: 'Email Invalid',
+  //   });
+  // }
   if (!req.body.password) {
     return res.status(400).send({
       message: 'Password is required',
@@ -51,6 +52,13 @@ const validateUserSignUp = (req, res, next) => {
       message: 'Only alphabets allowed in last name',
     });
   }
+  // if (!validator.isLength(email, { min: 2, max: 20 })) {
+  //   res.status(406)
+  //     .send({
+  //       status: 'Fail',
+  //       message: 'email should have 2 to 20 characters',
+  //     });
+  // }
   next();
 };
 
