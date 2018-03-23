c87416342a85c51d7fe9805ce03432439d376c42
ddouglasz <stevenzenne@gmail.com>
2018-03-23 14:47:55 +0100

    chore:setup variables for heroku deployment

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import routes from './server/routes/index';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(logger('dev'));
