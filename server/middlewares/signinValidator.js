027e7da0910b5058e79547b2ba1118311d92f3f5
ddouglasz <stevenzenne@gmail.com>
2018-03-23 13:04:55 +0100

    feature:modify sign in inputs validator to check for email and password

--- server/middlewares/signinValidator.js
+++ server/middlewares/signinValidator.js
@@ -5,13 +5,13 @@
    * @param {*} next
    */
 const validateUserSignIn = (req, res, next) => {
-  if (req.body.password === undefined || req.body.username === undefined) {
+  if (req.body.password === undefined || req.body.email === undefined) {
     return res.status(400)
       .json({
         message: 'All or some of the field is/are undefined',
       });
   }
-  if (!req.body.username) {
+  if (!req.body.email) {
     return res.status(400).send({
       message: 'username is can not be empty',
     });
