3d39f288da012308a5df8ceb9e10f3fcc8e59a56
ddouglasz <stevenzenne@gmail.com>
2018-03-27 21:42:12 +0100

    chore:write tests for sign in input fields and authorization

--- server/test/userTest.js
+++ server/test/userTest.js
@@ -1,17 +1,22 @@
 import chaiHttp from 'chai-http';
 import chai from 'chai';
 import app from '../../app';
-// import db from './../models';
+import models from './../models';
 
-// const { User } = db;
+const usersModel = models.users;
 const { expect, assert, should } = chai;
 should();
 
 chai.use(chaiHttp);
 
 describe('TEST for user routes', () => {
+  before((done) => {
+    usersModel
+      .sync({ force: true })
+      .then(() => done());
+  });
   describe('When user sends a POST request to /api/v1/auth/signup', () => {
-    it('should return a status 201 and a response message', () => {
+    it('should return a status 201 and a response message', (done) => {
       chai.request(app)
         .post('/api/v1/auth/signup')
         .send({
@@ -27,26 +32,108 @@ describe('TEST for user routes', () => {
             res.body.message,
             'Registration Successful'
           );
+          done();
         });
     });
 
-    // it('should return a status 201 and a response message', () => {
-    //   chai.request(app)
-    //     .post('/api/v1/auth/signup')
-    //     .send({
-    //       firstName: 'steve',
-    //       lastName: 'dougs',
-    //       email: 'steve@dougs.com',
-    //       password: 'password'
-    //     })
-    //     .end((err, res) => {
-    //       res.should.have.status(201);
-    //       expect(res.body).to.be.a('object');
-    //       assert.equal(
-    //         res.body.message,
-    //         'Registration Successful'
-    //       );
-    //     });
-    // });
+    it('should return a status 401 if required field is empty', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/signup')
+        .send({
+          firstName: 'steve',
+          lastName: 'dougs',
+          email: '',
+          password: 'password'
+        })
+        .end((err, res) => {
+          res.should.have.status(401);
+          expect(res.body).to.be.a('object');
+          assert.equal(
+            res.body.message,
+            'All or some of the field is/are undefined'
+          );
+          done();
+        });
+    });
+
+    it('should return a status 409 if duplicate mails exists', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/signup')
+        .send({
+          firstName: 'steve',
+          lastName: 'dougs',
+          email: 'steve@dougs.com',
+          password: 'password'
+        })
+        .end((err, res) => {
+          res.should.have.status(409);
+          expect(res.body).to.be.a('object');
+          assert.equal(
+            res.body.message,
+            'Email Already Exists'
+          );
+          done();
+        });
+    });
+  });
+
+  describe('When user sends a POST request to /api/v1/auth/login', () => {
+    it('should return a status 200 and a response message', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/login')
+        .send({
+          email: 'steve@dougs.com',
+          password: 'password'
+        })
+        .end((err, res) => {
+          res.should.have.status(200);
+          expect(res.body).to.be.a('object');
+          assert.equal(
+            res.body.message,
+            'signed in successfully'
+          );
+          assert.isString(
+            res.body.jwt,
+            'jwt should be a string'
+          );
+          done();
+        });
+    });
+
+    it('should return a status 401 if required field is empty', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/login')
+        .send({
+          email: '',
+          password: 'password'
+        })
+        .end((err, res) => {
+          res.should.have.status(401);
+          expect(res.body).to.be.a('object');
+          assert.equal(
+            res.body.message,
+            'All or some of the field is/are undefined'
+          );
+          done();
+        });
+    });
+
+    it('should return a status 404 if email is not in database', (done) => {
+      chai.request(app)
+        .post('/api/v1/auth/login')
+        .send({
+          email: 'steve@thomas.com',
+          password: 'password'
+        })
+        .end((err, res) => {
+          res.should.have.status(404);
+          expect(res.body).to.be.a('object');
+          assert.equal(
+            res.body.message,
+            'User Not Found'
+          );
+          done();
+        });
+    });
   });
 });
