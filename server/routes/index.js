5363bb543edd1f8fc4ab971fc1a3ea965f43748b
ddouglasz <stevenzenne@gmail.com>
2018-04-05 15:09:00 +0100

    feature/demo-feedback: modify file names and imports to obey naming conventions

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,20 +1,20 @@
-import Businesses from '../controllers/businesses';
+import businesses from '../controllers/businesses';
 import Users from '../controllers/users';
-import addBusinessValidator from '../middlewares/postBusinessValidator';
-import FilterBusinessSearch from '../middlewares/filterSearch';
-import ReviewsValidator from '../middlewares/postReviewValidator';
+import postBusinessValidator from '../middlewares/postBusinessValidator';
+import filterSearch from '../middlewares/filterSearch';
+import postReviewValidator from '../middlewares/postReviewValidator';
 import validateUserSignUp from '../middlewares/signUpValidator';
-import validateUserSignIn from '../middlewares/signinValidator';
+import signinValidator from '../middlewares/signinValidator';
 import auth from '../middlewares/auth';
 
 export default (app) => {
   app.post('/api/v1/auth/signup', validateUserSignUp, Users.signUp);
-  app.post('/api/v1/auth/login', validateUserSignIn, Users.signIn);
-  app.post('/api/v1/businesses', auth, addBusinessValidator, Businesses.createBusinesses);
-  app.get('/api/v1/businesses', FilterBusinessSearch.filterBusinessSearch, Businesses.getBusinesses);
-  app.delete('/api/v1/businesses/:businessId', auth, Businesses.deleteBusiness);
-  app.get('/api/v1/businesses/:businessId', Businesses.retrieveBusiness);
-  app.put('/api/v1/business/:businessId', auth, Businesses.updateBusiness);
-  app.post('/api/v1/businesses/:businessId/reviews', auth, ReviewsValidator.postReviewValidator, Businesses.postReview);
-  app.get('/api/v1/businesses/:businessId/reviews', Businesses.getReviews);
+  app.post('/api/v1/auth/login', signinValidator, Users.signIn);
+  app.post('/api/v1/businesses', auth, postBusinessValidator, businesses.createBusinesses);
+  app.get('/api/v1/businesses', filterSearch.filterBusinessSearch, businesses.getBusinesses);
+  app.delete('/api/v1/businesses/:businessId', auth, businesses.deleteBusiness);
+  app.get('/api/v1/businesses/:businessId', businesses.retrieveBusiness);
+  app.put('/api/v1/businesses/:businessId', auth, businesses.updateBusiness);
+  app.post('/api/v1/businesses/:businessId/reviews', auth, postReviewValidator.postReviewValidator, businesses.postReview);
+  app.get('/api/v1/businesses/:businessId/reviews', businesses.getReviews);
 };
