c51a094a1d8315edf08d39144533ebbe0b60db11
ddouglasz <stevenzenne@gmail.com>
2018-04-09 03:09:25 +0100

    chore: setup folders for clinet side setups

---
+++ client/src/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+  
+ReactDOM.render(
+    
+  <h1>Hello steve steve</h1>,
+
+  document.getElementById('app')
+
+);
+
+
