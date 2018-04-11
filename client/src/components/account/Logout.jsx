1684e1e4c2e50cc353e18a551dd32a383d51e2c7
ddouglasz <stevenzenne@gmail.com>
2018-04-11 23:01:57 +0100

    create a static Log out page to be rendered

---
+++ client/src/components/account/Logout.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {link} from 'react-router-dom';
+
+const Logout = () => {
+    return(
+<div><h1><strong>THANK YOU</strong></h1></div>
+    );
+}
+
+export default Logout;
