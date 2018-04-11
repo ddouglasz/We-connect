12184d279d0f3c2f480a2efe04888af398b70156
ddouglasz <stevenzenne@gmail.com>
2018-04-11 23:19:38 +0100

    feature:create a footer component to be rendered across all pages

---
+++ client/src/components/home/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {link} from 'react-router-dom';
+
+const Footer = () => {
+    return(
+      <footer className="bg-color footer">
+        <div className="col  ">
+            <div className="author float-left">
+              Designed by: Douglas &nbsp;&nbsp; © 2018
+            </div>
+            <a href="https://github.com/ddouglasz/We-connect/tree/master">
+               <hr />
+              <i className="fa fa-github float-right text-white ">
+                <strong>&nbsp;github</strong>
+              </i>
+            </a>
+        </div>
+      </footer>
+);
+}
+
+export default Footer;
