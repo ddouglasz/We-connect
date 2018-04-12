fc327c5c78b29af97e9ca409b43709010a46faed
ddouglasz <stevenzenne@gmail.com>
2018-04-12 15:42:40 +0100

    feature: modify css to make signup page uniform with login page

--- client/src/components/account/SIgnUp.jsx
+++ client/src/components/account/SIgnUp.jsx
@@ -7,64 +7,66 @@ const SignUp = () => {
     <div className="signup-page-background">
       <form className="form-signup form-spacing">
         <div className="row">
-          <div className="col-sm-12 ">
+          <div className="col-sm-12">
             <div className="form-actions">
               <h2 className="form-signup-heading text-white">Sign Up</h2>
             </div>
           </div>
         </div>
+<div class="signup-input-style">
         <div className="row">
           <div className="col-md-12">
             <div className="form-group form-spacing">
               <div className="input-group mb-2 mr-sm-2 mb-sm-0">
                 <div className="input-group-addon" style={{width: 2.6 + 'rem'}} >
-                  <i className="fa fa-user" />
+                  <i className="fa font-a fa-user" />
                 </div>
                 <input
                   type="text"
                   name="name"
-                  className="form-control text-whit"
-                  id="name1"
-                  placeholder="Name"
+                  className="form-control text-white"
+                  id="signup-firstName"
+                  placeholder="First Name"
                   required
                   autoFocus
                 />
               </div>
             </div>
           </div>
-          <div className="col-md-12">
-            <div className="form-group  form-spacing">
+
+<div className="col-md-12">
+            <div className="form-group form-spacing">
               <div className="input-group mb-2 mr-sm-2 mb-sm-0">
-                <div className="input-group-addon" style={{width: 2.6 + 'rem'}}>
-                  <i className="fa fa-at" />
+                <div className="input-group-addon" style={{width: 2.6 + 'rem'}} >
+                  <i className="fa font-a fa-user" />
                 </div>
                 <input
                   type="text"
-                  name="email"
-                  className="form-control text-whit"
-                  id="name2"
-                  placeholder="E-Mail Address"
+                  name="name"
+                  className="form-control text-white"
+                  id="signup-lastName"
+                  placeholder="Last Name"
                   required
                   autoFocus
                 />
               </div>
             </div>
           </div>
-        </div>
-        <div className="row">
+
           <div className="col-md-12">
             <div className="form-group  form-spacing">
               <div className="input-group mb-2 mr-sm-2 mb-sm-0">
                 <div className="input-group-addon" style={{width: 2.6 + 'rem'}}>
-                  <i className="fa fa-key" />
+                  <i className="fa font-a fa-at" />
                 </div>
                 <input
-                  type="password"
-                  name="password"
-                  className="form-control"
-                  id="name3"
-                  placeholder="Password"
+                  type="text"
+                  name="signup-email"
+                  className="form-control text-white"
+                  id="signup-email"
+                  placeholder="E-Mail Address"
                   required
+                  autoFocus
                 />
               </div>
             </div>
@@ -76,26 +78,29 @@ const SignUp = () => {
             <div className="form-group  form-spacing">
               <div className="input-group mb-2 mr-sm-2 mb-sm-0">
                 <div className="input-group-addon" style={{width: 2.6 + 'rem'}}>
-                  <i className="fa fa-repeat" />
+                  <i className="fa font-a fa-key" />
                 </div>
                 <input
+                  text-white
                   type="password"
-                  name="password"
-                  className="form-control"
-                  id="name4"
-                  placeholder="Confirm Password"
+                  name="signup-password"
+                  className="form-control text-white"
+                  id="signup-password"
+                  placeholder="Password*"
                   required
                 />
               </div>
             </div>
           </div>
         </div>
+        </div>
         <button type="submit" className="btn  btn-decor btn-success btn-block">
           <i className="fa font-a fa-user-plus" />
         </button>
       </form>
     </div>
-  </div>
+   </div>
+ 
 );
 }
 
