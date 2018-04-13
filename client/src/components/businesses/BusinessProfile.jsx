3183d6b19250f2dd4ca2bc3318527377bb8cc131
ddouglasz <stevenzenne@gmail.com>
2018-04-13 13:30:45 +0100

    feature: link business profile page to the user profile page

--- client/src/components/businesses/BusinessProfile.jsx
+++ client/src/components/businesses/BusinessProfile.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import {link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const BusinessProfile = () => {
-    return(
-
-        <div className="container">
-        <div className="form-actions">
-        </div>
+    return (
+    <div className="container">
+        <div className="form-actions" />
         <div className="row">
           <div className="col-sm-4">
             <br />
@@ -24,11 +22,11 @@ const BusinessProfile = () => {
                   </div>
                 </div>
                 <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="carousel-control-prev-icon" aria-hidden="true" />
                   <span className="sr-only">Previous</span>
                 </a>
                 <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="carousel-control-next-icon" aria-hidden="true" />
                   <span className="sr-only">Next</span>
                 </a>
               </div>
@@ -42,7 +40,7 @@ const BusinessProfile = () => {
               </div>
             </div>
           </div>
-    
+
           <div className="col-sm-8 personal-info">
             <div className="form-profile" id="description">
               <h3>Business Profile</h3>
@@ -51,31 +49,33 @@ const BusinessProfile = () => {
             <form className="form-horizontal" role="form">
               <div className="form-group form-spacing">
                 <label className="col-sm-6 control-label">
-                  <strong>Business Description:</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit unde necessitatibus
-                  eligendi facilis accusamus magnam ducimus, at mollitia maiores inventore cumque neque nobis! Blanditiis perspiciatis
-                  ex, ipsam iure voluptate quo sunt rem, dignissimos, quos minima at quisquam quidem laborum obcaecati illo.
-                  Sit libero deserunt error rem tempora cumque quaerat earum! 
-                  </label>
+                  <strong>Business Description:</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Velit unde necessitatibus eligendi facilis accusamus magnam ducimus, at mollitia maiores inventore cumque neque nobis! Blanditiis perspiciatis ex, ipsam iure voluptate quo sunt rem, dignissimos, quos minima at quisquam quidem laborum obcaecati illo. Sit libero deserunt error rem tempora cumque quaerat earum!
+                </label>
               </div>
               <div className="form-group form-spacing">
-                  <label className="col-sm-6 control-label">
-                    <strong>Business Added by:</strong> <a href="userProfile.html">@Mr.steve</a>
-                    </label>
-                </div>
+                <label className="col-sm-6 control-label">
+                 
+                <strong>Business Added by:</strong> 
+                <Link to="/userProfile" className="user-profile-link">
+                  <span> @Mr.steve</span>
+                </Link>
+                </label>
+
+              </div>
               <div className="form-group form-spacing">
                 <label className="col-sm-6 control-label">
                   <strong>Business Category:</strong> Online entertainment
-                  </label>
+                </label>
               </div>
               <div className="form-group form-spacing">
                 <label className="col-sm-6 control-label">
                   <strong>reviews:</strong> 256 reviews
-                  </label>
+                </label>
               </div>
               <div className="form-group form-spacing">
                 <label className="col-sm-6 control-label">
                   <strong>Business location:</strong> Lagos
-                  </label>
+                </label>
               </div>
               <div className="form-group form-spacing">
                 <label className="col-sm-6 control-label">
@@ -83,83 +83,75 @@ const BusinessProfile = () => {
                   <strong>
                     &nbsp;&nbsp;
                     <a href="#">
-                      <i className="fa fa-twitter    big-icon" id="twitter" aria-hidden="true">
-                      </i>
+                      <i className="fa fa-twitter    big-icon" id="twitter" aria-hidden="true" />
                     </a>
                     &nbsp;&nbsp;
                     <a href="#">
-                      <i className="fa fa-linkedin   big-icon" id="linkedin" aria-hidden="true">
-                      </i>
+                      <i className="fa fa-linkedin   big-icon" id="linkedin" aria-hidden="true" />
                     </a>
                     &nbsp;&nbsp;
                     <a href="#">
-                      <i className="fa fa-facebook   big-icon" id="facebook" aria-hidden="true">
-                      </i>
+                      <i className="fa fa-facebook   big-icon" id="facebook" aria-hidden="true" />
                     </a>
                   </strong>
                 </label>
               </div>
               <div className="form-group form-spacing">
-                <label className="col-sm-3 control-label"></label>
+                <label className="col-sm-3 control-label" />
                 <div className="col-sm-8">
-                  <a className="btn btn-primary blue-button" href="manageBusiness.html" role="button">Edit Business</a>
+                  <a className="btn btn-primary blue-button" href="manageBusiness.html" role="button">
+                    Edit Business
+                  </a>
                   <input type="reset" className="btn btn-danger" id="btn-delete" value="Delete Business" />
                 </div>
               </div>
               <div className="form-group form-spacing row">
-                <label for="smFormGroupInput" className="col-sm-12 col-form-label col-form-label-sm"></label>
+                <label for="smFormGroupInput" className="col-sm-12 col-form-label col-form-label-sm" />
                 <div className="col-sm-8" id="description">
-                  <textarea className="form-control" id="exampleTextarea" rows="3" placeholder="add a review"></textarea>
+                  <textarea className="form-control" id="exampleTextarea" rows="3" placeholder="add a review" />
                 </div>
               </div>
               <div className="form-group form-spacing">
-                <label className="col-sm-3 control-label"></label>
+                <label className="col-sm-3 control-label" />
                 <div className="col-sm-8">
-                  <a className="btn   btn-decor btn-primary blue-button" href="businessProfile.html" role="button">Post Review</a>
+                  <a className="btn   btn-decor btn-primary blue-button" href="businessProfile.html" role="button">
+                    Post Review
+                  </a>
                 </div>
               </div>
               <br />
               <div className="form-reviews" id="description">
                 <h3>Reviews</h3>
               </div>
-<div className="edit-spacing" id="chat-cards-buttom-spacing">
-              <ul className="list-unstyled">
-                <li className="media">
-                  <img className="d-flex mr-3" id="reviews-image" src="resources/images/bean.jpg" alt="Generic placeholder image" />
-                  <div className="media-body">
-                    <h5 className="mt-0 mb-1">List-based media object</h5>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-                    vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue
-                    felis in faucibus.
-                  </div>
-                </li>
-                <li className="media my-4">
+              <div className="edit-spacing" id="chat-cards-buttom-spacing">
+                <ul className="list-unstyled">
+                  <li className="media">
+                    <img className="d-flex mr-3" id="reviews-image" src="resources/images/bean.jpg" alt="Generic placeholder image" />
+                    <div className="media-body">
+                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                  </li>
+                  <li className="media my-4">
                     <img className="d-flex mr-3" id="reviews-image" src="resources/images/profileGlyph.jpg" alt="Generic placeholder image" />
-                  <div className="media-body">
-                    <h5 className="mt-0 mb-1">List-based media object</h5>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-                    vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue
-                    felis in faucibus.
-                  </div>
-                 </li>
-                <li className="media">
-                  <img className="d-flex mr-3" id="reviews-image" src="resources/images/beardedsmiley.jpg" alt="Generic placeholder image" />
-                  <div className="media-body">
-                    <h5 className="mt-0 mb-1">List-based media object</h5>
-                    Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in
-                    vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue
-                    felis in faucibus.
-                  </div>
-                </li>
-              </ul>
+                    <div className="media-body">
+                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                  </li>
+                  <li className="media">
+                    <img className="d-flex mr-3" id="reviews-image" src="resources/images/beardedsmiley.jpg" alt="Generic placeholder image" />
+                    <div className="media-body">
+                      <h5 className="mt-0 mb-1">List-based media object</h5>
+                      Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </div>
+                  </li>
+                </ul>
               </div>
-              </form>
+            </form>
           </div>
         </div>
       </div>
-      
-
-
     );
 }
 
