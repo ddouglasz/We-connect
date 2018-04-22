22e9f9809dc3a0d849d255ed5ef21a4325b889dd
ddouglasz <stevenzenne@gmail.com>
2018-04-22 23:57:20 +0100

    feature:add provider to routes

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,12 +3,22 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import styles from '../src/public/styles/main.scss'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+
+const store = createStore(
+  (state = {}) => state,
+  applyMiddleware(thunk)
+);
 
 ReactDOM.render(
   <div>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </div>,
   document.getElementById('app')
 );
