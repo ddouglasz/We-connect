050081cf385974bbe269e329535d6fee189ac54f
ddouglasz <stevenzenne@gmail.com>
2018-04-22 23:59:20 +0100

    feature:create actions for signup features

---
+++ client/src/actions/signupActions.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import decode from 'jwt-decode';
+import { CURRENT_USER } from './types';
+import setAuth from '../helper/setAuth';
+
+export const currentUser = user => ({
+  type: CURRENT_USER,
+  user,
+  isAuthenticated: true
+});
+
+/**
+ * Sign up
+ * @param {Object} userProfile
+ * @returns {object} action to be dispatched
+ */
+export const signupAction = userProfile => dispatch =>
+  axios.post('api/v1/auth/signup', userProfile)
+    .then((response) => {
+      const { token } = response.data;
+      setAuth(token);
+      localStorage.setItem('userToken', token);
+      const user = decode(token);
+      dispatch(currentUser(user));
+      return response.data.message;
+    });
