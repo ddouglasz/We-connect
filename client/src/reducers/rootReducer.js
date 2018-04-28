30a06143d198da77408f8a2fff09ab70454cc07e
ddouglasz <stevenzenne@gmail.com>
2018-04-28 20:35:46 +0100

    feature: add auth to rootReducer cobined reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,6 +1,11 @@
 import { combineReducers } from 'redux';
+
 import flashMessages from './flashMessages';
+import auth from './auth';
+
 
 export default combineReducers({
-  flashMessages
+  flashMessages,
+  auth
 });
+
