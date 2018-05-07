7bd24bbd2afaf076fb1466cd05d993b742600d13
ddouglasz <stevenzenne@gmail.com>
2018-05-07 22:05:51 +0100

    remove some files from .gitignore

---
+++ server/routes/router.js
@@ -0,0 +1,23 @@
+import businesses from '../controllers/businesses';
+import Users from '../controllers/users';
+import postBusinessValidator from '../middlewares/postBusinessValidator';
+import filterSearch from '../middlewares/filterSearch';
+import postReviewValidator from '../middlewares/postReviewValidator';
+import validateUserSignUp from '../middlewares/signUpValidator';
+import signinValidator from '../middlewares/signinValidator';
+import auth from '../middlewares/auth';
+
+export default (app) => {
+  setTimeout(() => {
+    app.post('/api/v1/auth/signup', validateUserSignUp, Users.signUp);
+    app.post('/api/v1/auth/login', signinValidator, Users.signIn);
+  }, 5000);
+  app.post('/api/v1/businesses', auth, postBusinessValidator, businesses.createBusinesses);
+  app.get('/api/v1/businesses', filterSearch.filterBusinessSearch, businesses.getBusinesses);
+  app.delete('/api/v1/businesses/:businessId', auth, businesses.deleteBusiness);
+  app.get('/api/v1/businesses/:businessId', businesses.retrieveBusiness);
+  app.put('/api/v1/businesses/:businessId', auth, businesses.updateBusiness);
+  app.post('/api/v1/businesses/:businessId/reviews', auth, postReviewValidator.postReviewValidator, businesses.postReview);
+  app.get('/api/v1/businesses/:businessId/reviews', businesses.getReviews);
+  app.get('/api/v1/businesses/:userId/userProfile', businesses.getUserProfile);
+};
