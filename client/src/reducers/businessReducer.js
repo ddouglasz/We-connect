62cccdfa66a56193d1a4ce3da14a2e8b52de5cee
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-6.local>
2018-05-14 01:34:05 +0100

    feature:create a reducer for businesses

---
+++ client/src/reducers/businessReducer.js
@@ -0,0 +1,11 @@
+import { ALL_BUSINESSES } from '../actions/types';
+
+export default function businesses(state = [], action = {}) {
+    switch (action.type) {
+        case ALL_BUSINESSES:
+        return action.allBusinesses
+
+        default:
+            return state;
+    }
+}
