7b749cebacc91f2a7b7be0acb0dbcff5bb381b96
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-6.local>
2018-05-16 00:03:41 +0100

    feature:add getonebusinessaction to reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import allBusinesses from  './businessReducer';
-
+import oneBusiness from './oneBusinessReducer';
 import flashMessages from './flashMessages';
 import auth from './auth';
 
@@ -8,6 +8,7 @@ import auth from './auth';
 export default combineReducers({
   flashMessages,
   allBusinesses,
+  oneBusiness,
   auth
 });
 
