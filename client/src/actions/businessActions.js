d456e5043891d35a6051239fa737b54c2be3620f
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-6.local>
2018-05-15 23:56:12 +0100

    feature: add getonebusiness action

--- client/src/actions/businessActions.js
+++ client/src/actions/businessActions.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
-import { ALL_BUSINESSES } from './types';
+import { ALL_BUSINESSES, ONE_BUSINESS } from './types';
 
 
 export function allBusinesses(businesses) {
   return {
-    type : ALL_BUSINESSES,
+    type: ALL_BUSINESSES,
     allBusinesses: businesses
+  };
+}
+
+export function oneBusiness(business) {
+  return {
+    type: ONE_BUSINESS,
+    oneBusiness: business
   }
 }
 
@@ -18,16 +25,20 @@ export const RegisterBusinessAction = businesses => dispatch =>
   axios.post('api/v1/businesses', businesses)
     .then(response => response.data.message);
 
- /**
+/**
  * Register a business
  * @param {*} businesses
  * @returns {object} action to be dispatched
  */
 export const getBusinessAction = businesses => dispatch =>
   axios.get('api/v1/businesses', businesses)
-    .then(response => {
-     dispatch(allBusinesses(response.data.businesses));
+    .then((response) => {
+      dispatch(allBusinesses(response.data.businesses));
     });
 
 
-// export default RegBusAction;
+export const getOneBusinessAction = id => dispatch =>
+  axios.get(`http://localhost:8000/api/v1/businesses/${id}`)
+    .then((response) => {
+      dispatch(oneBusiness(response.data.business));
+    });
