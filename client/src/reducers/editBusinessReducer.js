e20204b504e9e17ad9a96f9e60b5b449102172c2
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-6.local>
2018-05-20 21:38:10 +0100

    feature: create a reducer for edit business

---
+++ client/src/reducers/editBusinessReducer.js
@@ -0,0 +1,29 @@
+import { EDIT_SUCCESSFUL, EDIT_FAILED } from '../actions/types';
+const initialState = {
+    hasCreated: false,
+    hasError: false,
+    business: {},
+    error: {}
+}
+
+export default function businesses(state = initialState, action = {}) {
+    switch (action.type) {
+        case EDIT_SUCCESSFUL:
+        return {
+            hasCreated: true,
+            hasError: false,
+            business: action.business,
+            error: {}
+        };
+        case EDIT_FAILED:
+        return {
+            hasCreated: false,
+            hasError: true,
+            business: {},
+            error: action.error
+        };
+
+        default:
+            return state;
+    }
+}
