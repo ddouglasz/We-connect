016c405c09fd879f34445691b6fdb2f39dddc68b
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-6.local>
2018-05-20 21:36:47 +0100

    feature: create a reducer for edit business

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -3,12 +3,14 @@ import allBusinesses from  './businessReducer';
 import oneBusiness from './oneBusinessReducer';
 import flashMessages from './flashMessages';
 import auth from './auth';
+import editBusiness from './editBusinessReducer';
 
 
 export default combineReducers({
   flashMessages,
   allBusinesses,
   oneBusiness,
+  editBusiness,
   auth
 });
 
