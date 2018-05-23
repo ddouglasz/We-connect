4654881c0efa4208986601b9321d0714503aadcf
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-6.local>
2018-05-23 21:51:03 +0100

    feature: create actions for reviews

--- client/src/actions/reviewsActions.js
+++ client/src/actions/reviewsActions.js
@@ -1,15 +1,41 @@
 import axios from 'axios';
 import { ALL_REVIEWS, ADD_REVIEW_SUCCESS, ADD_REVIEW_FAILED } from './types';
+import ReviewsCard from '../components/businesses/ReviewsCards';
 
-export function allReviews(reviews) {
+export function allReviews(review) {
   return {
     type: ALL_REVIEWS,
-    allReviews: reviews
+    allReviews: review
   };
 }
+
 
 export const getReviewsAction = id => dispatch =>
   axios.get(`http://localhost:8000/api/v1/businesses/${id}/reviews`)
     .then((response) => {
-      dispatch(allReviews(response.data.reviews));
+      dispatch(allReviews(response.data.businessdata));
     });
+
+export function addReviewSuccess(review) {
+  return {
+    type: ADD_REVIEW_SUCCESS,
+    review
+  };
+}
+
+export function addReviewFailed(error) {
+  return {
+    type: ADD_REVIEW_FAILED,
+    error
+  };
+}
+
+export const postReviewAction = (review, id) => dispatch =>
+  axios.post(`http://localhost:8000/api/v1/businesses/${id}/reviews`, review)
+    .then(() => {
+      dispatch(addReviewSuccess('successfully added a review'));
+    })
+    // .catch((err) => {
+    //   console.log(err.data.message);
+    //   dispatch(addReviewFailed('hi'));
+    // });
