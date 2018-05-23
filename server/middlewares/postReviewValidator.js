608d2b0796c7350100b39fbc65e5a208ab900937
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-6.local>
2018-05-23 22:30:22 +0100

    feature: modify reviews validator. remove redundant codes

--- server/middlewares/postReviewValidator.js
+++ server/middlewares/postReviewValidator.js
@@ -19,23 +19,11 @@ class ReviewsValidator {
    */
   static postReviewValidator(req, res, next) {
     // check for contibutor
-    if (validator.isEmpty(req.body.review)) {
-      return res.status(400).json({
-        message: 'Name of of contributor is required',
-      });
-    }
     if (!validator.isLength(req.body.review, { min: 2, max: 100 })) {
       return res.status(406)
         .send({
           status: 'Fail',
           message: 'review content should have between 2 and 100 characters',
-        });
-    }
-    // check for reviews
-    if (validator.isEmpty(req.body.title)) {
-      return res.status(400)
-        .json({
-          message: 'review title can not be empty',
         });
     }
     next();
