d88a2ea78a798b2a80ba33c9271e745f0e836eeb
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-6.local>
2018-05-23 22:15:50 +0100

    feature: create reviews card

--- client/src/components/businesses/ReviewsCards.jsx
+++ client/src/components/businesses/ReviewsCards.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import shortId from 'shortid';
 
 const ReviewsCard = ({ reviews }) => {
   return (
-    <li className="media my-4">
-      <img className="d-flex mr-3" id="reviews-image" src={require('../../public/images/profileGlyph.jpg')} alt="Generic placeholder image" />
-      <div className="media-body">
-        <h5 className="mt-0 mb-1">List-based media object</h5>
-        {reviews}
-        </div>
-    </li>
+    reviews.map(review => (
+      <li className="media my-4" key={shortId.generate()}>
+        <img className="d-flex mr-3" id="reviews-image" src={require('../../public/images/profileGlyph.jpg')} alt="Generic placeholder image" />
+        <div className="media-body">
+          <h5 className="mt-0 mb-1">List-based media object</h5>
+          {review.review}
+          </div>
+      </li>
+    ))
   );
 }
 export default ReviewsCard;
