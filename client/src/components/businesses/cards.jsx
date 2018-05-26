52d3bc530e0f4f9d53521f0912d309e033526c64
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-6.local>
2018-05-26 16:35:44 +0100

    feature: modify images in business cards

--- client/src/components/businesses/cards.jsx
+++ client/src/components/businesses/cards.jsx
@@ -5,9 +5,11 @@ import TextTruncate from 'react-text-truncate';
 
 const Cards = ({ name, description, category, id, image }) => {
 return (
-    <div className="col-md-4 col-sm-6 col-xs-12">
+    <div className="col-md-4 col-sm-6 col-xs-12 business-card">
         <div className=" img-zoom card-space">
+        <div className="image-style" id="image-style1">
                  <img className="img img-fluid  " src={image} alt="Card image cap" width="537.5" />
+                 </div>
              <div className="card-block">
                 <h4 className="card-title detail-text">{name}</h4>
                 <TextTruncate className="card-title detail-text" text={description} lines={1}/>
