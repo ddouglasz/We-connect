2ea31405185696ad02c173b57475ec999030bc14
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-66.local>
2018-07-10 00:23:59 +0100

    feature: style userprofile page

--- client/src/components/user/UserProfile.jsx
+++ client/src/components/user/UserProfile.jsx
@@ -132,32 +132,6 @@ class UserProfile extends React.Component {
             </div>
           </div>
         </div>
-
-        {/* <div className="pagination-card btn1-spacing">
-          <nav aria-label="pages">
-            <ul className="pagination">
-              <li className="page-item disabled">
-                <a className="page-link" href="#" tabIndex="-1">Previous</a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">1</a>
-              </li>
-              <li className="page-item active">
-                <a className="page-link" href="#">2
-                <span className="sr-only">(current)</span>
-                </a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">3</a>
-              </li>
-              <li className="page-item">
-                <a className="page-link" href="#">Next</a>
-              </li>
-            </ul>
-          </nav>
-        </div> */}
-
-
       </div>
     );
   }
