6ffa8b43d1a2f0e10b7ebc880072e39068d5a3b7
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-66.local>
2018-07-11 15:46:02 +0100

    feature: spread reducer state to avoid mutation. combine reviews and pagination reducer in a single  switch method

--- client/src/reducers/businessReducer.js
+++ client/src/reducers/businessReducer.js
@@ -1,18 +1,41 @@
-import { ALL_BUSINESSES } from '../actions/types';
+import { ALL_BUSINESSES, PAGINATION, ONE_BUSINESS, ALL_REVIEWS } from '../actions/types';
 
 const initialState = {
   businesses: [],
-  pagination: {}
+  pagination: {},
+  oneBusiness: {},
+  allReviews: [],
 };
   /**
    * @param {object} state
    * @param {object} action
    * @return {object} state
    */
-export default function businesses(state = initialState, action = {}) {
+export default function businesses(state = initialState, action) {
   switch (action.type) {
     case ALL_BUSINESSES:
-      return action.allBusinesses;
+      return {
+        ...state,
+        businesses: action.businesses
+      };
+
+    case PAGINATION:
+      return {
+        ...state,
+        pagination: action.pagination
+      };
+
+    case ONE_BUSINESS:
+      return {
+        ...state,
+        oneBusiness: action.oneBusiness
+      };
+
+    case ALL_REVIEWS:
+      return {
+        ...state,
+        allReviews: action.allReviews
+      };
     default:
       return state;
   }
