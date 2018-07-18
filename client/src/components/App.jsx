6c76954600e7338f2b1fd33f482deedbc98d16d9
Douglas Egiemeh <douglasegiemeh@Andelas-MacBook-Pro-66.local>
2018-07-18 15:52:55 +0100

    feature: fix folder naming conflict

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Routes from '../Routes';
-import { Route, Switch } from 'react-router';
+// import { Route, Switch } from 'react-router';
 import NavBar from '../components/home/NavBar.jsx';
 import Footer from '../components/home/Footer.jsx';
-import styles from '../public/styles/main.scss'
-import flashMessages from '../reducers/flashMessages';
-import FlashMessageList from './flash/FlashMessagesList';
+// import styles from '../public/styles/main.scss';
+// import flashMessages from '../reducers/flashMessages';
+import FlashMessageList from './flash/FlashMessagesList.jsx';
 
 const App = () => (
   <div>
